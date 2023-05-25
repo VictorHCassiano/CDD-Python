3e3a96779a7bfa1c527ce3eaf3da273549275e06
Victor Hugo <104807646+VictorHCassiano@users.noreply.github.com>
2023-05-25 16:44:22 -0300

    Update herancaForma.py

--- ExercicioHeranca/herancaForma.py
+++ ExercicioHeranca/herancaForma.py
@@ -1,23 +1,37 @@
-class Forma():
-    def __init__(self,base,altura):
-       self.base = base
-       self.altura = altura
-class Retangulo(Forma):
-    def __init__(self, base, altura):
-        super().__init__(base, altura)
-    def calculaArea(self):
-            print(self.base*self.altura)
-    def calculaPerimetro(self):
-         print(2*(self.base+self.altura))
-class Triangulo(Forma):
-     def __init__(self, base, altura):
-          super().__init__(base, altura)
-     def calculaArea(self):
-          print((self.base*self.altura)/2)
-     def calculaPerimetro(self,lado1,lado2,lado3):
-          print(lado1+lado2+lado3)
+class Forma:
+    def __init__(self):
+        self.perimetro = 0
+        self.area = 0
 
-triangulo = Triangulo(10,10)
-retangulo = Retangulo(10,10)
-retangulo.calculaArea()
-triangulo.calculaArea()
+
+class Retangulo(Forma):
+    def __init__(self):
+        super().__init__()
+
+    def calculaArea(self,base,altura):
+        self.area = base * altura
+
+    def calculaPerimetro(self,base,altura):
+        self.perimetro = 2 * (base + altura)
+
+
+class Triangulo(Forma):
+    def __init__(self):
+        super().__init__()
+
+    def calculaArea(self,base,altura):
+        self.area = (base * altura) / 2
+
+    def calculaPerimetro(self, lado1, lado2, lado3):
+        self.perimetro = lado1 + lado2 + lado3
+
+
+triangulo = Triangulo()
+retangulo = Retangulo()
+retangulo.calculaArea(10, 10)
+retangulo.calculaPerimetro(10, 10)
+triangulo.calculaArea(10,10)
+triangulo.calculaPerimetro(5, 5, 5)
+
+print(triangulo.__dict__)
+print(retangulo.__dict__)
